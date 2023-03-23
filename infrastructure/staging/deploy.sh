3907ca4d9bfc68233bc15bf51013afc05f4c9b53
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-03-23 11:26:44 +0100

    infrastructure

---
+++ infrastructure/staging/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export $(grep -v '^#' ../../.env | xargs)
+
+echo $DOCKER_REGISTRY_PASSWORD | docker login --username $DOCKER_REGISTRY_USERNAME --password-stdin
+
+
+cd ../../.
+docker build -t kallepan/oe-frontend:dev -f Dockerfile.staging .
+docker push kallepan/oe-frontend:dev
+
+cd infrastructure/staging
+kubectl delete deployment oe-frontend -n genetics
+kubectl kustomize . > run.yaml
+kubectl apply -f run.yaml
