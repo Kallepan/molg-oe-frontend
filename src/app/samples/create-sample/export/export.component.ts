f7fe26fe9fcdaec30c309164b92d98e456aeb5c8
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-08-25 10:23:06 +0200

    fix: export timing

--- src/app/samples/create-sample/export/export.component.ts
+++ src/app/samples/create-sample/export/export.component.ts
@@ -38,11 +38,11 @@ export class ExportComponent {
   submit() {
     if (!this.authService.checkLoginWithDisplayMessage(ERRORS.ERROR_LOGIN)) return;
 
-    // Check if the current time is earlier than 15:00, if so show warning and return
+    // Check if the current time is earlier than 14:00 o clock, if so show warning and return
     const currentDate = new Date();
     const currentHour = currentDate.getHours();
-    if (currentHour < 15) {
-      this.messageService.simpleWarnMessage('Export kann erst ab 15:00 Uhr durchgeführt werden');
+    if (currentHour < 14) {
+      this.messageService.simpleWarnMessage('Export kann erst ab 14:00 Uhr durchgeführt werden');
       return;
     }
 
